1935044363ed7fbf8dc870e20480475b0b8adad0
paulcaron <31286140+paulcaron@users.noreply.github.com>
2018-10-02 21:45:20 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
-This project was completed inf the frame of Computer Science course INF473F by Marie-Paule Cani.
+#This project was completed inf the frame of Computer Science course INF473F by Marie-Paule Cani.
 It aims at developping a software solution to design 3D flying objects
 
 ![Alt text](Pictures/Example1.png?raw=true "Title")
