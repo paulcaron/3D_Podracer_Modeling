ff0607530635bcf1f8ae1e5fc18991bb4495b01f
paulcaron <31286140+paulcaron@users.noreply.github.com>
2018-10-02 21:44:00 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,4 @@
 This project was completed inf the frame of Computer Science course INF473F by Marie-Paule Cani.
 It aims at developping a software solution to design 3D flying objects
 
-![alt text](https://raw.githubusercontent.com/paulcaron/3D_Podracer_Modeling/Pictures/Example1.png)
+![Alt text](Pictures/Example1.jpg?raw=true "Title")
